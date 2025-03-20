4bcf086711086bcef70a028284507f7f2818074b
Marakas <igorpetriv.bsc@gmail.com>
2025-03-20 20:47:55 +0200

    timer anded

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,39 +1,70 @@
-
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
-
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import "flatpickr/dist/themes/dark.css";
-import { convertMs, addLeadingZero } from './helpers'
+import { convertMs, addLeadingZero, errorMessage, buttonDisabledToggle } from './helpers'
 
+// GLOBAL
 let timerID = null;
 const startButton = document.querySelector(".button");
 
+// add disable btn styles
+buttonDisabledToggle(startButton,'#7a3136')
+
+// FLATPICR LIBRARY IMPLEMENT
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
+  // CLOSING BEHAVIOR LOGIC FOR ONCLOSE METHOD
   onClose(selectedDates) {
     
     const endDate = new Date(selectedDates[0].getTime());
     const now = new Date().getTime();
     
-    if (endDate - now < 0) { 
-      alert('rgf')
-      return
-    }    
-    
+    if (endDate - now < 0) {
+      errorMessage("Please choose a date in the future");
+      return;
+    }
+    //make btn active
+    startButton.removeAttribute('disabled')
+    buttonDisabledToggle(startButton,'#7a3136')
+    // remove prev interval, add new and start timer
     startButton.removeEventListener('click', startTimer);
-    startButton.addEventListener('click', () => startTimer(endDate));
+    startButton.addEventListener('click', () => {
+      startTimer(endDate);
+      // make btn disabled
+      startButton.setAttribute('disabled', 'true')
+      buttonDisabledToggle(startButton,'#7a3136') 
+    });
   
   }   
 }
 flatpickr("#datetime-picker", options);
 
 
+//START TIMER LOGIC
+function startTimer(endDate) {
+      // if prev timer exist
+      if (timerID) clearInterval(timerID);
+      // start timer
+      timerID = setInterval(() => {
+      let now = new Date().getTime();
+      const timeLeft = endDate - now;
+      // if time on timer ends
+      if (timeLeft <= 0) {
+        clearInterval(timerID);
+        displayTimer(0, 0, 0, 0);
+        return;
+        }
+      // convert time to d.h.m.s and adding to html 
+      let { days, hours, minutes, seconds } = convertMs(endDate - now);
+      displayTimer(days, hours, minutes, seconds);
+        
+      }, 1000)
+    }
 
+//ADDING TIME TO HTML
 function displayTimer(days, hours, minutes, seconds) {
   document.querySelector('[data-days]').textContent = addLeadingZero(days) ;
   document.querySelector('[data-hours]').textContent = addLeadingZero(hours);
@@ -42,22 +73,3 @@ function displayTimer(days, hours, minutes, seconds) {
 }
 
 
-function startTimer(endDate) {
-
-      if (timerID) clearInterval(timerID);
-
-      timerID = setInterval(() => {
-      let now = new Date().getTime();
-      const timeLeft = endDate - now;
-
-      if (timeLeft <= 0) {
-        clearInterval(timerID);
-        displayTimer(0, 0, 0, 0);
-        return;
-        }
-        
-        let { days, hours, minutes, seconds } = convertMs(endDate - now);
-        displayTimer(days, hours, minutes, seconds);
-        
-      }, 1000)
-    }
--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,10 +1,13 @@
-// ----------------FORMATING DATE-------------------
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+// ------------------------FORMATING DATE--------------------------
 
 export function addLeadingZero(value) {
   return String(value).padStart(2, "0");
 }
 
-// --------------------CONVERT FROM MS-------------------------
+// ------------------------CONVERT FROM MS-------------------------
 
 
 export function convertMs(ms) {
@@ -24,4 +27,33 @@ export function convertMs(ms) {
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
+}
+
+// ------------------ERROR MESSAGE FOR TIMER--------------------------
+
+export function errorMessage(message) {
+ iziToast.error({
+    
+   message: message,
+   position: 'topLeft',
+   transitionIn: 'fadeInDown',
+   backgroundColor: 'rgb(231, 19, 36)',
+   messageColor: 'white',
+   iconColor: 'white',
+   messageSize: '16px',
+   class: 'toast',
+});
+}
+
+// --------------ADDING AND REMOVE DISABLE STYLES FOR BTN----------------
+
+
+export function buttonDisabledToggle(element, color) {
+  if (element.disabled) {
+    element.style.pointerEvents = "none";
+    element.style.background = color;
+  } else {
+    element.style.removeProperty("pointer-events");
+    element.style.removeProperty("background");
+  }
 }
